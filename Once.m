a875bdcf8126210c0adaa8f2bc7160e44b8064d7
Dark204 <69815160+Dark204@users.noreply.github.com>
2020-08-18 02:34:29 +0800

    Update Once.m

--- Once.m
+++ Once.m
@@ -2,10 +2,10 @@ function [G_mean,AUC,Balance]=Once(data,km,numHN,C)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%test_sensitivity, test_specificity, test_gmean
 [NumS,NumF]=size(data);  
 indices=zeros(1,NumS);
-rand_samples=rand(1,NumS);   %先把样本集顺序打乱
-[r1,r2]=sort(rand_samples);  %对矩阵rand_samples进行排序，返回的r1是rand_samples升序排列后的值，r2是索引值，就是r1中对应位置的值在原来rand_samples中的位置
-temp=floor(NumS/10);          %floor向负无穷大方向取整
-for i=1:temp                 %五折交叉可以这么做，对每个样本赋予一个折标签
+rand_samples=rand(1,NumS);   
+[r1,r2]=sort(rand_samples);  
+temp=floor(NumS/10);          
+for i=1:temp                 
    indices(1,r2(1,i))=1;
 end
 for i=(temp+1):(2*temp)
@@ -36,16 +36,16 @@ for i=(9*temp+1):NumS
    indices(1,r2(1,i))=10;
 end
 
-for i=1:10             %用for循环顺序运行每一折
-   test = (indices==i);  %本次循环用对应标签作为测试样本的折数
+for i=1:10             
+   test = (indices==i);  
    train =~test;
-   train_input=data(train,:); %剩余九折放训练集里
-   test_input=data(test,:);  %下面几行把训练样本集和测试样本集中的训练数据和类标分离
+   train_input=data(train,:); 
+   test_input=data(test,:);  
 
 
 % WELM_DenE
-   train_input1=cal_same(train_input);%去重
-   [dataG,weight]=DenE(train_input1,km); %把训练集放到DenE函数中，重新调整训练样本排列顺序，并计算对应权重
+   train_input1=cal_same(train_input);
+   [dataG,weight]=DenE(train_input1,km); 
    [test_gmean(1,i),test_AUC(1,i),test_balance(1,i)] = ELM_regularized_LXL(dataG,test_input,1, numHN,'sig',C, weight);
    clear train_input1;
    clear dataG;
